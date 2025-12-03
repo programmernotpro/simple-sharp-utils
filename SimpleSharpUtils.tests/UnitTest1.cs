b113444404a3a83b600692be4ab9930fe27bd260
programmernotpro <199767981+programmernotpro@users.noreply.github.com>
2025-12-03 23:44:56 +0200

    Initial code. Contains just the .Reverse() method

---
+++ SimpleSharpUtils.tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using SimpleSharpUtils;
+using NUnit.Framework.Legacy;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    //Test for .Reverse()
+    public void Test1() {
+
+        string expected = "hello";
+        {
+            Assert.That(expected,Is.EqualTo(Utilities.Reverse("hello")));
+        }
+    }
+}
