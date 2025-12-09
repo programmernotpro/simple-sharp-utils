3e0a65edbb8f42662ccac3862e5802dd98a316e3
programmernotpro <199767981+programmernotpro@users.noreply.github.com>
2025-12-09 18:54:20 +0200

    Refactor directory tests to use static methods

--- SimpleSharpUtils.tests/Directorytests.cs
+++ SimpleSharpUtils.tests/Directorytests.cs
@@ -1,50 +1,50 @@
 using NUnit.Framework;
 using FileUtils;
 using System.IO;
+using NUnit.Framework.Internal.Execution;
+using NUnit.Framework.Internal;
 
 namespace SimpleSharpUtils.Tests
 {
     public class DirectoryTests
     {
         [Test]
-        public void GetFilesNumber_Returns_File_Count()
+        public static void GetFilesNumberTest()
         {
-            int count = FileUtils.Directory.GetFilesNumber(TestFilesConfig.BaseDir);
+            int expected = 2;
+            Assert.That(UtilsDirectory.GetFilesNumber(TestFilesConfig.BaseDir), Is.EqualTo(expected));
+        
 
-            Assert.That(count, Is.AtLeast(1), "hello.txt should be in directory");
+        }
+        [Test]
+        public static void GetFilesTest()
+        {
+            string[] expected = [".md",".txt"];
+            Assert.That(UtilsDirectory.GetExtensions(TestFilesConfig.BaseDir), Is.EqualTo(expected));   
         }
 
         [Test]
-        public void GetFiles_Returns_Array()
+        public static void GetDirSize()
         {
-            string[] files = FileUtils.Directory.GetFiles(TestFilesConfig.BaseDir);
-
-            Assert.That(files.Length, Is.GreaterThan(0));
+            Assert.That(UtilsDirectory.GetDirSize(TestFilesConfig.BaseDir), Is.LessThan(0.5));
         }
 
         [Test]
-        public void GetFiles_First1_Returns_Single_File()
+        public static void HasFileTest()
         {
-            string[] files = FileUtils.Directory.GetFiles(TestFilesConfig.BaseDir, 1);
-
-            Assert.That(files.Length, Is.EqualTo(1));
+            Assert.That(UtilsDirectory.HasFile(TestFilesConfig.BaseDir,"hello.txt"), Is.EqualTo(true));
         }
 
         [Test]
-        public void GetFilesMulti_Returns_Dict_With_Valid_Entry()
+        public static void GetLargestFileTest()
         {
-            var result = FileUtils.Directory.GetFilesMulti(new[] { TestFilesConfig.BaseDir });
-
-            Assert.That(result.ContainsKey(TestFilesConfig.BaseDir), Is.True);
-            Assert.That(result[TestFilesConfig.BaseDir].Length, Is.GreaterThan(0));
+            Assert.That(UtilsDirectory.GetLargestFile(TestFilesConfig.BaseDir), Is.EqualTo("hello.md"));
         }
 
         [Test]
-        public void GetFilesNumberMulti_Returns_Correct_Total()
+        public static void GetSmallestFileTest()
         {
-            long total = FileUtils.Directory.GetFilesNumberMulti(new[] { TestFilesConfig.BaseDir });
-
-            Assert.That(total, Is.GreaterThan(0));
+            Assert.That(UtilsDirectory.GetSmallestFile(TestFilesConfig.BaseDir), Is.EqualTo("hello.txt"));
         }
     }
 }
